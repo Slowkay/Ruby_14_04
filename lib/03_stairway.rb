e29287eac95cc21b9f9b2b716901a9d4fa5426cc
slowkay <robin.guesdon@gmail.com>
2021-04-15 10:33:42 +0200

    Revoir exo 3

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,13 +1,13 @@
-marche = 0
+# marche = 0
 
-def dice
-  des = rand(6) + 1
-  return des
-end
+# def dice
+#   des = rand(6) + 1
+#   return des
+# end
 
-def etat(marche)
-  puts "Tu es sur la marche #{number}"
-end 
+# def etat(marche)
+#   puts "Tu es sur la marche #{number}"
+# end 
 
 def start
   puts "Ecrivez START pour lancer le jeu !"
@@ -19,35 +19,90 @@ def start
   end 
 end
 
-def game(marche)
-  while marche == 10
-    puts "Victoire !"
-  end
-end
+# def game(marche)
+#   while marche == 10
+#     puts "Victoire !"
+#   end
+# end
 
-def dess(dice, marche)
-  if des == 1
-    puts "Vous reculez !"
-    return marche -= 1
+# def dess(dice, marche)
+#   if des == 1
+#     puts "Vous reculez !"
+#     return marche -= 1
   
-  elsif des == 2 || des == 3 || des == 4
-    puts "Vous restez ici !"
-    return marche += 0
+#   elsif des == 2 || des == 3 || des == 4
+#     puts "Vous restez ici !"
+#     return marche += 0
   
-  else des == 5 || des == 6
-    puts "Vous avancez !"
-    return marche += 1
-  end
-end
+#   else des == 5 || des == 6
+#     puts "Vous avancez !"
+#     return marche += 1
+#   end
+# end
 
-def program
-  while 
+# def program
+#   while 
 
-def all
-  start
-  etat(1)
+# def all
+#   start
+#   etat(1)
   
 
-end
+# end
  
-all
+# all
+
+# ****************************************
+
+# Lancer un dé virtuel
+def roll_dice
+  return rand(6) + 1
+end
+
+# Analiser le dé
+def analyze_dice(dice)
+  if dice >= 5
+    puts "Vous avancez !"
+    return 1
+  elsif dice == 1
+    puts "Vous reculez !"
+    return -1
+  else 
+    puts "rien ne se passe"
+    return 0
+  end
+end
+
+# Afficher un état des lieux 
+def show_state(num)
+  puts "Vous êtes sur la marche n° #{num}"
+end
+
+# Vérifier si la partie doit se finir 
+def is_over?(num)
+  if num == 10
+    return true
+  else 
+    return false
+  end 
+end 
+
+# Lier les méthodes entre elles
+def play 
+  start
+
+  step = 1
+  show_state(step)
+
+  while(!is_over?(step)) do
+    puts "Appuyez sur 'entrée' pour jouer !"
+    gets.chomp
+    step += analyze_dice(roll_dice)
+    show_state(step)
+  end
+  
+  puts " "
+  puts "==========VICTOIRE=========="
+end
+
+play
